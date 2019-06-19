6688a90a1a7a6f45fa91bd22671d156f92e89eac
wakeupmh <wakeupmh@gmail.com>
2019-06-19 12:27:13 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const Twitter = require('twit');
+const config = require('./config');
+const sentiment = require('multilang-sentiment')
+const tClient = new Twitter(config);
+ 
+tClient.get('account/verify_credentials', { skip_status: true })
+  .catch(function (err) {
+    console.log('caught error', err.stack)
+  })
+  .then(function (result) {
+    // console.log('data', result.data);
+  })
+
+  const stream = tClient.stream('statuses/filter', { track: '#RatoMoroTaMelindrado', language: 'pt' })
+ 
+  stream.on('tweet', function (tweet) {
+    let user = tweet.user;
+    console.log('  hastag pesquisada: #RatoMoroTaMelindrado\n\n');
+    console.log(`  ${user.name}<@${user.screen_name}, followers:${user.followers_count}, location:${user.location}>: ${tweet.text}\n`);
+    let tweetSentiment = sentiment(tweet.text, 'pt');
+    // console.dir(tweetSentiment);
+    if(tweetSentiment.score < 0)
+      console.log(`  A impressão desse tweet é positiva frente a hashtag\n\n`);
+    else if(tweetSentiment.score > 0)
+      console.log(  `A impressão desse tweet é negativa frente a hashtag\n\n`);
+    console.log('_________________________________________________________________________\n\n');
+    
+  })
